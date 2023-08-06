019ec7ada330dfedf1d695e3d91b9177bcad45ae
Robert Joachim Encinas <102633935+Eun-DLSU@users.noreply.github.com>
2023-08-06 20:31:46 +0800

    Update server.js

--- Source Code/server.js
+++ Source Code/server.js
@@ -47,6 +47,7 @@ mongoose.connect(mongo)
     .then(async () => {
         console.log('Connected to MongoDB!')
         const indices = await Counter.findOne({})
+        console.log(indices)
         indexerId = indices._id
         commentIndex = indices.commentIndex
         postIndex = indices.postIndex
